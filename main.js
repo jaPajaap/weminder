3dff91ac7cbd70fce53d231b9072079aeff08785
Jaap Gerritsen <jaapgerritsen@gmail.com>
2016-07-10 17:07:27 +0200

    fix deps

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import createBrowserHistory from 'history/lib/createBrowserHistory'
 import createStore from './store/createStore'
 import App from './components/App/App'
 var css = require('./app.css');
